d067a3bfcc65bfa828e64e74f20b062b81363177
julian libor <julian.libor@gmail.com>
2018-06-19 13:11:51 +0200

    remove python changes

--- server.py
+++ server.py
@@ -94,17 +94,26 @@ class MyHTTPHandler(BaseHTTPRequestHandler):
 
         if(self.path == "/stopSvm"):
             print("post /stopSvm")
-            ### POST Request Header ###
-            self.send_response(200)
-            self.send_header('Content-type', 'application/json')
-            self.end_headers()
+                ### POST Request Header ###
+                self.send_response(200)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
 
-            # Katjas code goes here
-            group = katja_function(data.p, data.n)
+                # get body from request
+                #content_len = int(self.headers['Content-Length'])
+                #body = self.rfile.read(content_len)
 
-            # make json
-            data = json.dumps(group).encode()
-            self.wfile.write(group)  #body zurueckschicken
+                # convert body to list
+                #data = json.loads(str(body).decode('utf-8'))  # python 2
+                #data = json.loads(str(body, encoding='utf-8'))      # python 3
+                #print(data)
+
+                # Katjas code goes here
+                p, n = katja_function(data.p, data.n)
+
+                # make json
+                #data = json.dumps({p: p, n: n}).encode()
+                self.wfile.write("stopped Svm")  #body zurueckschicken
 
 
         return
