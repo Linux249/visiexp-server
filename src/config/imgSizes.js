13a4c74be8ad07c74ed8e40e833072e7a194cfe9
julian libor <julian.libor@gmail.com>
2019-01-24 17:45:21 +0100

    resizePics.js: take config/imgPath as default path and other improvementa

--- src/config/imgSizes.js
+++ src/config/imgSizes.js
@@ -1,17 +1,19 @@
 export const imgSizes = [
-    10,     // zoomStage: 0
-    20,     // zoomStage: 1
-    30,     // zoomStage: 2
-    40,     // zoomStage: 3
-    50,     // zoomStage: 4
-    60,     // zoomStage: 5
-    70,     // zoomStage: 6
-    80,     // zoomStage: 7
-    90,     // zoomStage: 8
-    100,    // zoomStage: 9
-    110,    // zoomStage: 10
-    120,    // zoomStage: 11
-    130,    // zoomStage: 12
-    140,    // zoomStage: 13
-    150,    // zoomStage: 14
+    10, // zoomStage: 0
+    20, // zoomStage: 1
+    30, // zoomStage: 2
+    40, // zoomStage: 3
+    50, // zoomStage: 4
+    60, // zoomStage: 5
+    70, // zoomStage: 6
+    80, // zoomStage: 7
+    90, // zoomStage: 8
+    100, // zoomStage: 9
+    110, // zoomStage: 10
+    120, // zoomStage: 11
+    130, // zoomStage: 12
+    140, // zoomStage: 13
+    150, // zoomStage: 14
 ];
+
+export default imgSizes;
--- src/util/resizePics.js
+++ src/util/resizePics.js
@@ -1,64 +1,74 @@
 // import data from '../../mock/2582_sub_wikiarts';
-import sharp from 'sharp';
 import fs from 'fs';
-import { imgSizes as sizes } from '../config/imgSizes';
-import {dataSet} from "../config/datasets";
+import path from 'path';
+import sharp from 'sharp';
+import sizes from '../config/imgSizes';
+import imagePath from '../config/imgPath';
+import { dataSet } from '../config/datasets';
+
+const fsp = fs.promises;
 
 const resizePics = async (imgPath, imgSizes) => {
-    console.log('start resizePics');
+    console.log(`path: ${imgPath}`);
     console.time('resizePics');
+    const timeResizePics = process.hrtime();
 
     // check the hole path
     if (!fs.existsSync(imgPath)) return new Error('Pfad zu Bilder in resizePics ungültig');
     // create all dirs
     imgSizes.forEach((size) => {
-        const dir = `${imgPath}${size}/`;
-        console.log(dir);
+        const dir = path.join(imgPath, size.toString());
+        // console.log(dir);
         if (!fs.existsSync(dir)) fs.mkdirSync(dir);
-        else console.warn('dir allready exists - what to do now is not implemented');
+        else console.warn('dir allready exists - doing nothing now');
     });
 
     // console.log(imgPath);
-    await fs.readdir(imgPath, async (err, files) => {
-        if (err) return new Error(err);
-
+    try {
+        const files = await fsp.readdir(imgPath);
+        const count = files.length;
+        console.log(`start resizing ${count} pics`);
         // map through files
-        await Promise.all(files.map((file, i) => {
+        await Promise.all(files.map(async (file, i) => {
             // check if file is a folder (10, 20, ...)
             if (imgSizes.includes(+file)) return null;
             // path to load img
-            const sourceImagePath = `${imgPath}${file}`;
+            const sourceImagePath = path.join(imgPath, file);
 
-            const pic = sharp(sourceImagePath);
+
             // map through image sizes
-            return imgSizes.map((size) => {
-                const outPath = `${imgPath}${size}/${file.split('.')[0]}.png`;
+            return Promise.all(imgSizes.map((size) => {
+                const outPath = path.join(imgPath, size.toString(), `${file.split('.')[0]}.png`);
                 // if file exists return
                 if (fs.existsSync(outPath)) return null;
                 // todo chekc sharp newest version, issue #1153 seems to add a new method for alpha
-                return pic.resize(size, size)
+                return sharp(sourceImagePath)
                     .resize(size, size, { fit: 'inside' })
                     .ensureAlpha()
                     .toFile(outPath)
                     .then((_) => {
-                        if (i && !(i % 100)) console.log(`saved: ${i} - ${outPath}`);
+                        if (size === 10 && i && !(i % 100)) {
+                            const diffResizePics = process.hrtime(timeResizePics);
+                            console.log(`saved: ${i}/${count} in ${diffResizePics[0] + diffResizePics[1] / 1e9}s (- )${outPath})`);
+                        }
                     })
                     .catch((e) => {
                         console.error(e);
                         console.log({ file, path: sourceImagePath, outPath });
                     });
-            });
+            }));
         }));
-    });
+    } catch (err) {
+        if (err) return new Error(err);
+    }
     console.timeEnd('resizePics');
 };
 
 
 export default resizePics;
 
-const path = 'C:/Users/libor/bachelor-node/images/2582_sub_wikiarts#90/';
-console.log(path);
-resizePics(path, sizes).then((e) => {
+const testPath = 'C:/Users/libor/bachelor-node/images/2582_sub_wikiarts#90/';
+resizePics(imagePath, sizes).then((e) => {
     console.log('Finished: all images resized');
 }).catch((err) => {
     console.error('Error: resizePics not finished');
