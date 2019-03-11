67cb1cd1fc7c09458ca3a7ce1241c80bdfb693b4
julian libor <julian.libor@gmail.com>
2019-03-11 15:05:50 +0100

    bash script: add create dir

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-path="/net/hci-storage02/groupfolders/compvis/datasets/Animals_with_Attributes2/"
-#subfolder=ls ${path}/JPEGImages/
-
+path="/net/hci-storage02/groupfolders/compvis/datasets/Animals_with_Attributes2"
+newDir=${path}"/single_folder_images2/"
+mkdir newDir
+echo "created: " newDir
 for d in $(find ${path}/JPEGImages -maxdepth 1 -type d)
 do
   #Do something, the directory is accessible with $d:
   echo ${d}
+  #ln -s ${d}/* ${path}/single_folder_images2/
 done
 
 #ln -s ${path}/JPEGImages/antelope/* ${path}/single_folder_images2/
