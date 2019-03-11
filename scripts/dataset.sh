fd7f15c369bab069bac37c690ab1733999349671
julian libor <julian.libor@gmail.com>
2019-03-11 15:02:14 +0100

    bash script: add loop

--- scripts/dataset.sh
+++ scripts/dataset.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 path="/net/hci-storage02/groupfolders/compvis/datasets/Animals_with_Attributes2/"
+#subfolder=ls ${path}/JPEGImages/
 
-ln -s $path/JPEGImages/antelope/* $path/single_folder_images2/	
+for d in $(find ${path}/JPEGImages -maxdepth 1 -type d)
+do
+  #Do something, the directory is accessible with $d:
+  echo ${d}
+done
+
+#ln -s ${path}/JPEGImages/antelope/* ${path}/single_folder_images2/
