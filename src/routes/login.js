f7c64a77bdf31378849b5f37bd2bf48e8c1c12bd
julian libor <julian.libor@gmail.com>
2019-06-04 17:19:07 +0200

    add login route

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,5 +1,6 @@
+import md5 from 'md5';
 import { Router } from 'express';
-import { db } from '../config/db';
+import { db } from '../config/db_secret';
 
 const router = Router();
 
@@ -29,6 +30,14 @@ router.post('/login', async (req, res, next) => {
     console.log({ user, password });
     if (!user) return res.status(504).json({ message: 'user missing' });
     if (!password) return res.status(504).json({ message: 'password missing' });
+    connection.query('SELECT * FROM vis_users WHERE username = ? AND password = ?', [user, md5(password)], (error, results, fields) => {
+        if (results.length > 0) {
+            response.json({isAuth: true});
+        } else {
+            response.status(504).json({message: 'Incorrect Username and/or Password!'});
+        }
+
+    });
     return res.json({ users: [{ name: 'Timmy' }] });
 });
 
