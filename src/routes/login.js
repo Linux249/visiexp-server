a12871632c4cd6295a4978e572f8d869b20e9fb8
julian libor <julian.libor@gmail.com>
2019-07-10 19:48:12 +0200

    bedder login errors

--- src/routes/login.js
+++ src/routes/login.js
@@ -29,22 +29,22 @@ router.post('/login', async (req, res, next) => {
     console.log(req.body);
     const { user, password } = req.body;
     console.log({ user, password });
-    if (!user) return res.status(504).json({ message: 'user missing' });
-    if (!password) return res.status(504).json({ message: 'password missing' });
+    if (!user) return res.json({ message: 'user missing' });
+    if (!password) return res.json({ message: 'password missing' });
+    if (connection.state === 'disconnected') return res.json({ message: 'database connection missing' });
     try {
         connection.query('SELECT * FROM vis_users WHERE name = ? ', [user], (error, results, fields) => {
             console.log(results);
-            if (error) return res.json(error);
+            if (error) return next(error);
             if (results.length > 0) {
                 // check pw
-                if (results[0].password !== md5(password)) return res.status(504).json({ message: 'Incorrect Password!' });
-                res.json({ isAuth: true, id: results[0].id, user: results[0].name });
-            } else {
-                res.status(504).json({ message: 'Incorrect Username' });
+                if (results[0].password !== md5(password)) return res.json({ message: 'Incorrect Password!' });
+                return res.json({ isAuth: true, id: results[0].id, user: results[0].name });
             }
+            return res.json({ message: 'Incorrect Username' });
         });
     } catch (e) {
-        return res.json(e);
+        return next(e);
     }
 });
 
