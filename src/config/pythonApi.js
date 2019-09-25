6f92ee557d62d875487c662cc868fb3af93c9872
julian libor <julian.libor@gmail.com>
2019-09-25 12:22:06 +0200

    switch python backend server

--- src/config/pythonApi.js
+++ src/config/pythonApi.js
@@ -1,6 +1,6 @@
 // export const pythonApi = 'http://hcigpu01:8000'; // (process.env.NODE_ENV === 'development') ? 'localhost' : '129.206.117.75';
 // export const pythonApi = 'http://129.206.117.181:8000'; // HCI GPU01
-export const pythonApi = 'http://129.206.117.193:8000'; // HCI GPU03
-// export const pythonApi = 'http://localhost:8000';
+// export const pythonApi = 'http://129.206.117.193:8000'; // HCI GPU03
+export const pythonApi = 'http://localhost:8000';
 
 export default pythonApi;
