149b785909683b73cdae6dc0256a0ec73715043f
julian libor <julian.libor@gmail.com>
2019-11-19 22:03:10 +0100

    update python api

--- src/config/pythonApi.js
+++ src/config/pythonApi.js
@@ -1,10 +1,10 @@
 import devMode from './env';
 // export const pythonApi = 'http://hcigpu01:8023'; // (process.env.NODE_ENV === 'development') ? 'localhost' : '129.206.117.75';
 // export const pythonApi = 'http://129.206.117.181:8000'; // HCI GPU01
-// export const pythonApi = 'http://129.206.117.193:8023'; // HCI GPU03
+export const pythonApi = 'http://129.206.117.193:8023'; // HCI GPU03
 
-export const pythonApi = devMode
-  ? 'http://compvis10.iwr.uni-heidelberg.de:8023'
-  : 'http://127.0.0.1:8023';
+// export const pythonApi = devMode
+//   ? 'http://compvis10.iwr.uni-heidelberg.de:8023'
+//   : 'http://127.0.0.1:8023';
 
 export default pythonApi;
